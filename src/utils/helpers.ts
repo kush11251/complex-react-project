5e454fa60d17baeb9bfa71efe7d934a6b8b8c299
Kussagra Pathak <67318693+kush11251@users.noreply.github.com>
2026-03-18 03:57:33 +0530

    feat: integrate src/utils/helpers.ts

---
+++ src/utils/helpers.ts
@@ -0,0 +1,11 @@
+/**
+ * Helpers provides utility functions
+ */
+const Helpers = {
+  /**
+   * Logs a message to the console
+   * @param {string} message
+   */
+  logMessage: (message: string) => console.log(message)
+};
+export { Helpers };
